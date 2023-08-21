3fd38ee4afc7c65f362cd6a1d2c419f1e43b64db
ThaiPham <thaib2012257@student.ctu.edu.vn>
2023-08-21 18:02:14 +0700

    connect db, fix error
    connect db, fix error

--- HRM_backend/src/main/java/com/project/hrm/Controllers/AuthController.java
+++ HRM_backend/src/main/java/com/project/hrm/Controllers/AuthController.java
@@ -9,6 +9,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class AuthController {
     @PostMapping(URLConfigs.REGISTER)
     public Object getAllUser() {
-        return Object;
+        return new String("");
     }
 }
