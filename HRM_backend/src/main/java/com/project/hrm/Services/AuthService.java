e43f0e14c1a91f8929e210e0e9dcd44ffe542da8
ThaiPham <75441539+B2012257@users.noreply.github.com>
2023-08-21 18:19:25 +0700

    test (#3)
    1

--- HRM_backend/src/main/java/com/project/hrm/Services/AuthService.java
+++ HRM_backend/src/main/java/com/project/hrm/Services/AuthService.java
@@ -1,5 +1,12 @@
 package com.project.hrm.Services;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ResponseBody;
+
 public interface AuthService {
-    public Object Register();
+    public ResponseEntity Register();
+    public ResponseEntity Login();
+    public ResponseEntity Logout();
+    public ResponseEntity RfToken();
+
 }
