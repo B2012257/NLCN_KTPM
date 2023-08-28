ac0afa8d82b9dd2bac41b1fac735ce840322552d
ThaiPham <thaib2012257@student.ctu.edu.vn>
2023-08-29 02:40:38 +0700

    fix confict urlconfig

--- HRM_backend/src/main/java/com/project/hrm/Configs/URLConfigs.java
+++ HRM_backend/src/main/java/com/project/hrm/Configs/URLConfigs.java
@@ -31,5 +31,7 @@ public class URLConfigs {
     public static final String GET_INFO_STAFF ="/infoStaff";
     public static final String ADD_STAFF ="/addStaff";
     public static final String GET_ALL_STAFF ="/allStaff";
-
+    public static final String ADD_SALARY = "/addSalary";
+    public static final String GET_ALL_SALARY = "salaries";
+    public static final String EDIT_SALARY = "editSalary";
 }
