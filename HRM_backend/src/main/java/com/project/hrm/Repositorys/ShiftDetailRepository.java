add0332684e58ce98a3dd3531c321d02ab9048d4
ThaiPham <thaib2012257@student.ctu.edu.vn>
2023-09-26 23:20:49 +0700

    FIx

--- HRM_backend/src/main/java/com/project/hrm/Repositorys/ShiftDetailRepository.java
+++ HRM_backend/src/main/java/com/project/hrm/Repositorys/ShiftDetailRepository.java
@@ -3,8 +3,10 @@ package com.project.hrm.Repositorys;
 import com.project.hrm.Models.Date;
 import com.project.hrm.Models.Shift;
 import com.project.hrm.Models.ShiftDetail;
-import com.project.hrm.Models.Staff;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
 
