a7506cd628ffc34e25132331363e05b5d08d629f
hoangphong02 <hoangphongvl2002@gmail.com>
2023-10-02 17:15:57 +0700

    phong _fontend

--- HRM_backend/src/main/java/com/project/hrm/Models/Staff.java
+++ HRM_backend/src/main/java/com/project/hrm/Models/Staff.java
@@ -28,7 +28,7 @@ public class Staff {
 
     @NotNull
     private String phone;
-    private String gender;
+//    private String gender;
     private Date beginWork;
     private String location;
     private String bankName;
@@ -55,7 +55,7 @@ public class Staff {
         this.bankAccount = newStaff.getBankAccount();
         this.type = newStaff.getType();
         this.urlAvatar = newStaff.getUrlAvatar();
-        this.gender = newStaff.getGender();
+//        this.gender = newStaff.getGender();
 
     }
 
