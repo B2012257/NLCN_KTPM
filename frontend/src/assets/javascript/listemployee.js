ea49c8d500ad37940d2f4eb26beb3df5a6c09dff
hoangphong02 <hoangphongvl2002@gmail.com>
2023-10-14 22:49:08 +0700

    staff and listStaff

--- frontend/src/assets/javascript/listemployee.js
+++ frontend/src/assets/javascript/listemployee.js
@@ -1,8 +1,10 @@
 const api = `http://localhost:8081/api/v1/manager/allStaff`;
 const apiType = `http://localhost:8081/api/v1/manager/types`;
-
+const getInfoStaff = `http://localhost:8081/api/v1/manager/infoStaff`;
 let data = [];
 
+let avatar = "";
+let dbAvatar;
 function start() {
   getStaff(function (fetchedData) {
     data = fetchedData;
@@ -72,7 +74,6 @@ function getType() {
 
 function renderStaff(staffs) {
   const listStaff = document.getElementById("list-staff");
-  const modal = document.getElementById("modal");
   var htmls = staffs.map(function (staff) {
     const typeName = staff.type ? staff.type.name : "";
 
@@ -89,7 +90,7 @@ function renderStaff(staffs) {
     const month = months > 0 ? `${months}` : "0";
     const year = years > 0 ? `${years} năm ` : "";
     const returnHTML = `
-        <tr style="text-align: center" role="button" title="Bấm để xem chi tiết" data-bs-toggle="modal"
+        <tr style="text-align: center" role="button" title="Bấm để xem chi tiết" data-bs-toggle="modal" id="${staff.uid}"
         data-bs-target="#modal">
         <td>${staff.uid}</td>
         <td>${staff.fullName}</td>
@@ -109,15 +110,18 @@ function renderStaff(staffs) {
     return returnHTML;
   });
   listStaff.innerHTML = htmls.join("");
-}
 
-// function removeDiacriticsAndSpaces(str) {
-//   return str
-//     .normalize("NFD")
-//     .replace(/[\u0300-\u036f]/g, "")
-//     .replace(/\s/g, "")
-//     .toLowerCase();
-// }
+  let idStaff = document.querySelectorAll('tr[data-bs-toggle="modal"]');
+  console.log("idStaff", idStaff);
+
+  idStaff.forEach((tr) => {
+    tr.addEventListener("click", function () {
+      const value = tr.getAttribute("id");
+
+      getInfoDetailStaff(value);
+    });
+  });
+}
 
 function filterStaffByRole(data, role) {
   // console.log("Role selected:", role);
@@ -133,16 +137,6 @@ function filterStaffByRole(data, role) {
     renderStaff(filteredStaff);
   }
 }
-// const radioButtons = document.querySelectorAll(
-//   'input[name="inlineRadioOptions"]'
-// );
-
-// radioButtons.forEach((radio) => {
-//   radio.addEventListener("change", function () {
-//     console.log("Selected value:", this.value);
-//     filterStaffByRole(data, this.value); // Truyền thêm tham số data vào hàm filterStaffByRole
-//   });
-// });
 
 function filterStaffByName(data, searchTerm) {
   return data.filter((staff) => {
@@ -158,3 +152,187 @@ btnSearch.addEventListener("click", function () {
   const filteredStaff = filterStaffByName(data, searchTerm);
   renderStaff(filteredStaff);
 });
+
+async function getInfoDetailStaff(uid) {
+  try {
+    const response = await fetch(`${getInfoStaff}?uid=${uid}`, {
+      method: "GET",
+      mode: "cors",
+      credentials: "include",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    const responseData = await response.json();
+
+    if (responseData) {
+      const data = responseData.data;
+      console.log(data);
+      fillStaffInfo(data);
+    }
+  } catch (error) {
+    console.error("Error:", error);
+  }
+}
+
+const idGetDetail = "";
+function fillStaffInfo(data) {
+  document.getElementById("userId").value = data.uid;
+  document.getElementById("FullName").value = data.fullName;
+  document.getElementById("gender").value = data.gender;
+  document.getElementById("type").value = data.type.name;
+  document.getElementById("phone").value = data.phone;
+  document.getElementById("location").value = data.location;
+  document.getElementById("bankAccount").value = data.bankAccount;
+  document.getElementById("bankName").value = data.bankName;
+  document.getElementById("avatar").src = data.urlAvatar;
+  document.getElementById("beginWork").value = data.beginWork;
+  dbAvatar = data.urlAvatar;
+}
+console.log("idGetDetail", idGetDetail);
+function enableEdit() {
+  const inputElements = document.querySelectorAll(".editInfo");
+  inputElements.forEach((inputElement) => {
+    inputElement.removeAttribute("disabled");
+  });
+
+  const buttonCompleted = document.getElementById("buttonCompleted");
+  buttonCompleted.style.display = "block";
+
+  const buttonEdit = document.getElementById("buttonEdit");
+  buttonEdit.style.display = "none";
+
+  const buttonCancel = document.getElementById("buttonCancel");
+  buttonCancel.style.display = "block";
+
+  document.getElementById("upload_widget").addEventListener(
+    "click",
+    function () {
+      myWidget.open();
+    },
+    false
+  );
+}
+var myWidget = cloudinary.createUploadWidget(
+  {
+    cloudName: "dfcjwhc7o",
+    uploadPreset: "cdootl7q",
+  },
+  (error, result) => {
+    if (!error && result && result.event === "success") {
+      console.log("Done! Here is the image info: ", result.info);
+      let url = result.info.url;
+      avatar = url;
+    }
+  }
+);
+
+function cancelInfo() {
+  const buttonCompleted = document.getElementById("buttonCompleted");
+  buttonCompleted.style.display = "none";
+  const buttonCancel = document.getElementById("buttonCancel");
+  buttonCancel.style.display = "none";
+  const buttonEdit = document.getElementById("buttonEdit");
+  buttonEdit.style.display = "block";
+  const inputElements = document.querySelectorAll(".editInfo");
+  inputElements.forEach((inputElement) => {
+    inputElement.setAttribute("disabled", "disabled");
+  });
+  const value = document.getElementById("userId").value;
+  console.log("value", value);
+  getInfoDetailStaff(value);
+}
+
+function updateStaffInfoHandler() {
+  const newFullName = document.getElementById("FullName").value;
+  const newGender = document.getElementById("gender").value;
+  const newPhone = document.getElementById("phone").value;
+  const newLocation = document.getElementById("location").value;
+  const newBankAccount = document.getElementById("bankAccount").value;
+  const newBankName = document.getElementById("bankName").value;
+  const uid = document.getElementById("userId").value;
+  const beginWork = document.getElementById("beginWork").value;
+  const dataUpdate = {};
+
+  if (avatar !== "") {
+    dataUpdate.urlAvatar = avatar;
+    console.log(avatar);
+  } else {
+    dataUpdate.urlAvatar = dbAvatar;
+  }
+
+  dataUpdate.fullName = newFullName;
+  dataUpdate.gender = newGender;
+  dataUpdate.phone = newPhone;
+  dataUpdate.location = newLocation;
+  dataUpdate.bankAccount = newBankAccount;
+  dataUpdate.bankName = newBankName;
+
+  dataUpdate.beginWork = beginWork;
+
+  dataUpdate.uid = uid;
+
+  console.log(uid);
+  console.log(dataUpdate);
+
+  updateStaffInfo(dataUpdate);
+}
+
+//Gọi api cập nhật thông tin cá nhân
+function updateStaffInfo(dataUpdate) {
+  fetch("http://localhost:8081/api/v1/staff/editStaff", {
+    method: "PUT",
+    mode: "cors",
+    credentials: "include",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(dataUpdate),
+  })
+    .then((res) => res.json())
+    .then((res) => {
+      console.log(res);
+      if (res.status === "OK") {
+        console.log(res);
+        return location.reload();
+      }
+
+      return console.log(res);
+    })
+
+    .then((data) => {
+      console.log("User data updated successfully:", data);
+      alert("Thông tin đã được cập nhật thành công."); // Thông báo khi cập nhật thành công
+    })
+    .catch((error) => console.error("Error:", error));
+}
+
+async function deleteStaff(uid) {
+  try {
+    const response = await fetch(
+      `http://localhost:8081/api/v1/manager/deleteStaff/${uid}`,
+      {
+        method: "DELETE",
+        mode: "cors",
+        credentials: "include",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+    const responseData = await response.json();
+    if (responseData.status === "OK") {
+      return location.reload();
+    }
+  } catch (error) {}
+}
+
+function loadFullName() {
+  const fullName = document.getElementById("userNameStaffDelete");
+  const newFullName = document.getElementById("FullName").value;
+  fullName.textContent = newFullName;
+}
+function onClickDeleteStaff() {
+  const userId = document.getElementById("userId").value;
+  deleteStaff(userId);
+}
